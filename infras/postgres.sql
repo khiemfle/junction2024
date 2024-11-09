9674a6e6838b9f130f34615d0dc038892ee9388a
khiemfle <khiemfle@gmail.com>
2024-11-10 00:55:50 +0200

    feat: add postgres for alert system

--- infras/postgres.sql
+++ infras/postgres.sql
@@ -1,13 +1,30 @@
+DROP TABLE IF EXISTS feedback CASCADE;
+DROP TABLE IF EXISTS predefined_feedback CASCADE;
+DROP TABLE IF EXISTS batch_state CASCADE;
+DROP TABLE IF EXISTS package CASCADE;
+DROP TABLE IF EXISTS batch CASCADE;
+DROP TABLE IF EXISTS sector CASCADE;
+DROP TABLE IF EXISTS sector_type CASCADE;
+DROP TABLE IF EXISTS product CASCADE;
+
+
+-- Table: sector_type
+CREATE TABLE sector_type (
+    id TEXT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT
+);
+
 -- Table: product
 CREATE TABLE product (
-    id SERIAL PRIMARY KEY,
+    id TEXT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     image TEXT
 );
 
 -- Table: sector
 CREATE TABLE sector (
-    id SERIAL PRIMARY KEY,
+    id TEXT PRIMARY KEY,
     location VARCHAR(255),
     name VARCHAR(255) NOT NULL,
     sector_type_id INTEGER NOT NULL,
@@ -16,7 +33,7 @@ CREATE TABLE sector (
 
 -- Table: batch
 CREATE TABLE batch (
-    id SERIAL PRIMARY KEY,
+    id TEXT PRIMARY KEY,
     product_id INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE
@@ -24,7 +41,7 @@ CREATE TABLE batch (
 
 -- Table: batch_state
 CREATE TABLE batch_state (
-    id SERIAL PRIMARY KEY,
+    id TEXT PRIMARY KEY,
     batch_id INTEGER NOT NULL,
     sector_id INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -34,7 +51,7 @@ CREATE TABLE batch_state (
 
 -- Table: package
 CREATE TABLE package (
-    id SERIAL PRIMARY KEY,
+    id TEXT PRIMARY KEY,
     product_id INTEGER NOT NULL,
     sector_id INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -42,16 +59,9 @@ CREATE TABLE package (
     FOREIGN KEY (sector_id) REFERENCES sector(id) ON DELETE CASCADE
 );
 
--- Table: sector_type
-CREATE TABLE sector_type (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT
-);
-
 -- Table: predefined_feedback
 CREATE TABLE predefined_feedback (
-    id SERIAL PRIMARY KEY,
+    id TEXT PRIMARY KEY,
     product_id INTEGER NOT NULL,
     sector_type_id INTEGER NOT NULL,
     severity_level INTEGER NOT NULL,
@@ -63,7 +73,7 @@ CREATE TABLE predefined_feedback (
 
 -- Table: feedback
 CREATE TABLE feedback (
-    id SERIAL PRIMARY KEY,
+    id TEXT PRIMARY KEY,
     predefined_feedback_id INTEGER NOT NULL,
     batch_state_id INTEGER NOT NULL,
     package_id INTEGER NOT NULL,
